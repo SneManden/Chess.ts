5f3195d3bb23cc1e00f81d31094f3b72e6e58710
Casper Kehlet Jensen <casper@snemanden.com>
2021-02-04 21:32:53 +0100

    Add a test to make sure check works

--- test/pieces/King_test.ts
+++ test/pieces/King_test.ts
@@ -15,3 +15,12 @@ Deno.test("king cannot move to squares under attack", () => {
   rookAt(board, Color.Black, Position.create("A5"));
   assertMovesDoesNotContain(king.validMoves(), ["D5", "C5", "E5"]);
 });
+
+Deno.test("king (white) is check should restrict possible moves for white", () => {
+  const board = new Board();
+  const king = kingAt(board, Color.White, Position.create("D4"));
+  const rookW = rookAt(board, Color.White, Position.create("A5"));
+  rookAt(board, Color.Black, Position.create("D8"));
+  assertMovesDoesNotContain(king.validMoves(), ["D5", "D3"]); // Must move away from column D under attack
+  assertMovesEquals(rookW.validMoves(), ["D5"]); // Only valid move is to protect king
+});
