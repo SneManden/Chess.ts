a039525e115b3035fd5217de2eadccab1ab795aa
Casper Kehlet Jensen <casper@snemanden.com>
2021-02-05 21:36:21 +0100

    Missed from previous commit

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Game } from "./Game.ts";
+import { Game, GameOptions } from "./Game.ts";
 import { RandomPlayer } from "./players/RandomPlayer.ts";
 import { GreedyPlayer } from "./players/GreedyPlayer.ts";
 import { HumanPlayer } from "./players/HumanPlayer.ts";
@@ -10,14 +10,15 @@ const p2 = new GreedyPlayer("GreedyPlayer1");
 const p1 = new RandomPlayer("RandomPlayer2");
 
 const hasHuman = p1.name.startsWith("Human");
-
-game.setupNewGame(p1, p2);
-
-const results = await game.startGame({
+const options: GameOptions = {
   delay: hasHuman ? 1_000 : null,
   drawBoard: hasHuman,
   maxRounds: 200,
-});
+};
+
+game.setupNewGame(p1, p2);
+
+const results = await game.startGame(options);
 
 console.log("Results:", {
   winner: results.winner === "draw" ? "draw" : results.winner.name,
